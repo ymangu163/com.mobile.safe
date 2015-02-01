b353ff71e03e17599745c47c8a78b039aa8df024
ymangu163 <ymangu@163.com>
2015-02-01 10:38:56 +0800

    ① 解析Json，用JsonObject ② 进入主界面或升级逻辑 ③进入主界面动画

--- src/com/mobile/safe/SplashActivity.java
+++ src/com/mobile/safe/SplashActivity.java
@@ -6,19 +6,28 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.util.LogUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.mobile.safe.activity.HomeActivity;
 import com.mobile.safe.utils.StreamTools;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /*
 	 * splash 界面的作用
@@ -32,6 +41,11 @@ public class SplashActivity extends Activity {
 	@ViewInject(R.id.tv_splash_version)
 	private TextView tv_splash_version; 
 	
+	protected static final int SHOW_UPDATE_DIALOG = 0;
+	protected static final int ENTER_HOME = 1;
+	protected static final int URL_ERROR = 2;
+	protected static final int NETWORK_ERROR = 3;
+	protected static final int JSON_ERROR = 4;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +57,47 @@ public class SplashActivity extends Activity {
 		
 		// 检查升级
 		checkUpdate();
+		AlphaAnimation aa = new AlphaAnimation(0.2f, 1.0f);
+		aa.setDuration(1000);
+		findViewById(R.id.rl_root_splash).startAnimation(aa);  // 给RelativeLayout 设置动画
 		
 	}
+	
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case SHOW_UPDATE_DIALOG:// 显示升级的对话框
+				LogUtils.i("显示升级的对话框");
+//				showUpdateDialog();
+				break;
+			case ENTER_HOME:// 进入主页面
+				enterHome();
+				break;
+			case URL_ERROR:// URL错误
+				enterHome();
+				Toast.makeText(getApplicationContext(), "URL错误", 0).show();
+
+				break;
+
+			case NETWORK_ERROR:// 网络异常
+				enterHome();
+				Toast.makeText(SplashActivity.this, "网络异常", 0).show();
+				break;
+
+			case JSON_ERROR:// JSON解析出错
+				enterHome();
+				Toast.makeText(SplashActivity.this, "JSON解析出错", 0).show();
+				break;
+
+			default:
+				break;
+			
+			
+			}
+			
+			
+		};
+	};
 	
 	
 	/**
@@ -53,7 +106,12 @@ public class SplashActivity extends Activity {
 	private void checkUpdate() {
 
 		new Thread(){
+			private String description;
+			private String apkurl;
+
 			public void run() {
+				Message mes = Message.obtain();
+				long startTime = System.currentTimeMillis();
 				// URL http://192.168.1.254:8080/updateinfo.html
 				try {
 					URL url=new URL(getString(R.string.servelurl));
@@ -71,24 +129,62 @@ public class SplashActivity extends Activity {
 							String result = StreamTools.readFromStream(is);
 							LogUtils.i("联网成功了" + result);
 						
+							// json解析
+							JSONObject obj = new JSONObject(result);
+							// 得到服务器的版本信息
+							String version = (String) obj.get("version");
+							description = (String) obj.get("description");
+							apkurl = (String) obj.get("apkurl");
+							
+							// 校验是否有新版本
+							if (getVersionName().equals(version)) {
+								// 版本一致，没有新版本，进入主页面
+								mes.what = ENTER_HOME;
+							} else {
+								// 有新版本，弹出一升级对话框
+								mes.what = SHOW_UPDATE_DIALOG;
+
+							}
 						
-						}
-						
+						}						
 					} catch (IOException e) {
+						mes.what = NETWORK_ERROR;
 						e.printStackTrace();
-					}
-					
+					} catch (JSONException e) {
+						mes.what = JSON_ERROR;
+						e.printStackTrace();
+					}					
 				} catch (MalformedURLException e) {
+					mes.what = URL_ERROR;
 					e.printStackTrace();
+				}finally {
+					long endTime = System.currentTimeMillis();
+					// 我们花了多少时间
+					long dTime = endTime - startTime;
+					// 2000
+					if (dTime < 2000) {
+						try {
+							Thread.sleep(2000 - dTime);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}					
+					handler.sendMessage(mes);
 				}
 				
 				
-			};
-			
-			
-			
+			};			 
 		}.start();
 		
+	}
+
+
+
+	protected void enterHome() {
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);
+		// 关闭当前页面
+		finish();		
 	}
 
 
