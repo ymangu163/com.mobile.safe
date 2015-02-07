9bc1582c3f29e3125c3d94f8d7dd9ae42f9d25af
ymangu163 <ymangu@163.com>
2015-02-07 16:16:51 +0800

    设置是否开启防盗保护

--- src/com/mobile/safe/activity/Setup4Activity.java
+++ src/com/mobile/safe/activity/Setup4Activity.java
@@ -1,5 +1,7 @@
 package com.mobile.safe.activity;
 
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 import com.mobile.safe.R;
 
 import android.app.Activity;
@@ -8,16 +10,53 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class Setup4Activity extends BaseSetupActivity {
 	
 	private SharedPreferences sp;
-
+	@ViewInject(R.id.cb_proteting)
+	private CheckBox cb_proteting;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup4);		
 		sp = getSharedPreferences("config", MODE_PRIVATE);
+		ViewUtils.inject(this);
+		
+		boolean  protecting = sp.getBoolean("protecting", false);
+		if(protecting){
+			//手机防盗已经开启了
+			cb_proteting.setText("手机防盗已经开启");
+			cb_proteting.setChecked(true);
+		}else{
+			//手机防盗没有开启
+			cb_proteting.setText("手机防盗没有开启");
+			cb_proteting.setChecked(false);
+			
+		}
+		cb_proteting.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				if(isChecked){
+					cb_proteting.setText("手机防盗已经开启");
+				}else{
+					cb_proteting.setText("手机防盗没有开启");
+				}
+				
+				//保存选择的状态
+				Editor editor = sp.edit();
+				editor.putBoolean("protecting", isChecked);
+				editor.commit();
+			}
+		});
+		
 	}
 
 	@Override
