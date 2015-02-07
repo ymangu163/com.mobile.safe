c5b5d4126645af7e02af9e000323346dcb762b7e
ymangu163 <ymangu@163.com>
2015-02-07 18:07:45 +0800

    ① 判断安全号码 ②播放报警音乐-raw

--- src/com/mobile/safe/receiver/SMSReceiver.java
+++ src/com/mobile/safe/receiver/SMSReceiver.java
@@ -1,56 +1,71 @@
 package com.mobile.safe.receiver;
 
 import com.lidroid.xutils.util.LogUtils;
+import com.mobile.safe.R;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver {
-
+	private SharedPreferences sp;
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// 写接收短信的代码
 		Object[] objs = (Object[]) intent.getExtras().get("pdus");
-		
+		sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
 		for(Object b:objs){
 			//得到具体的某一条短信
 			SmsMessage sms =SmsMessage.createFromPdu((byte[]) b);
 			//得到 发送者
 			String sender = sms.getOriginatingAddress();//15555555556
-//			String safenumber = sp.getString("safenumber", "");//5556
+			Toast.makeText(context, sender, 1).show();
+			String safenumber = sp.getString("safenumber", "");//5556
 			String body = sms.getMessageBody();  //得到短信的内容
 			
-			switch (body) {
-			case "#*location*#":   //得到手机的GPS				
-				LogUtils.i( "得到手机的GPS");
-				
-				//把这个广播终止掉,不让手机用户看到短信
-				abortBroadcast();
-				break;
-			case "#*alarm*#":  	//播放报警影音
-				LogUtils.i("播放报警影音");
-				
-				abortBroadcast();
-				break;
-			case "#*wipedata*#":  	//远程清除数据
-				LogUtils.i("远程清除数据");
-				
-				abortBroadcast();
-				break;
-			case "#*lockscreen*#":  	//远程锁屏
-				LogUtils.i("远程锁屏");
-				
-				abortBroadcast();
-				break;
-				
-			default:
-				break;
+			if (sender.contains(safenumber)) {   //判断是不是来自安全号码
+
+				switch (body) {
+				case "#*location*#": // 得到手机的GPS
+					LogUtils.i("得到手机的GPS");
+
+					// 把这个广播终止掉,不让手机用户看到短信
+					abortBroadcast();
+					break;
+				case "#*alarm*#": // 播放报警影音
+					LogUtils.i("播放报警影音");
+					MediaPlayer player = MediaPlayer.create(context, R.raw.ylzs);
+					player.setLooping(false);//true的话就循环播放
+					/*
+					 * .如果没播放多媒体是调音量改变的是打电话等铃声音量；
+					 *  如果在播放多媒体时，改变的是多媒体音量，这时才没有声音
+					 */
+					player.setVolume(1.0f, 1.0f);  //设置左右声道的音量大小 为最大 
+					player.start();					
+					
+					abortBroadcast();
+					break;
+				case "#*wipedata*#": // 远程清除数据
+					LogUtils.i("远程清除数据");
+
+					abortBroadcast();
+					break;
+				case "#*lockscreen*#": // 远程锁屏
+					LogUtils.i("远程锁屏");
+
+					abortBroadcast();
+					break;
+
+				default:
+					break;
+				}
+
 			}
-			
 		}
-		
 		
 		
 	}
