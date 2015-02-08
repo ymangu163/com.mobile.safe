ee6ad124b1f748713e535f955807257c190c7176
ymangu163 <ymangu@163.com>
2015-02-08 23:21:56 +0800

    EditText 的抖动效果 及 振动效果

--- src/com/mobile/safe/activity/NumberAddressQueryActivity.java
+++ src/com/mobile/safe/activity/NumberAddressQueryActivity.java
@@ -2,10 +2,13 @@ package com.mobile.safe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,18 +26,22 @@ public class NumberAddressQueryActivity extends Activity {
 	@ViewInject(R.id.result)
 	private TextView result;
 	
+	/**
+	 * 系统提供的振动服务
+	 */
+	private Vibrator vibrator;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_number_addres_query);
 		ViewUtils.inject(this);
 		
+		vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 		ed_phone.addTextChangedListener(new TextWatcher() {
 			//文本发生变化之前回调
 			@Override
 			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-				
+					int after) {				
 			}
 			//当文本发生变化的时候回调
 			@Override
@@ -67,6 +74,16 @@ public class NumberAddressQueryActivity extends Activity {
 		String phone = ed_phone.getText().toString().trim();
 		if(TextUtils.isEmpty(phone)){
 			Toast.makeText(this, "号码为空", 0).show();
+			 Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+			 ed_phone.startAnimation(shake);
+			 
+			 //当电话号码为空的时候，就去振动手机提醒用户
+//			 vibrator.vibrate(2000);  //振动2s
+			 long[] pattern = {200,200,300,300,1000,2000};   //有规律的振动:停 振动，停 振动
+			 //-1不重复 0循环振动 1；
+			 vibrator.vibrate(pattern, -1);
+			
+			
 			return;
 		}else{
 			String address = NumberAddressQueryUtils.queryNumber(phone);
