622eea81595669396bd5b92fcddfa84232729f7b
ymangu163 <ymangu@163.com>
2015-02-25 23:22:52 +0800

    短信备份， ProgressDialog显示进度

--- src/com/mobile/safe/activity/AtoolsActivity.java
+++ src/com/mobile/safe/activity/AtoolsActivity.java
@@ -1,20 +1,22 @@
 package com.mobile.safe.activity;
 
 import com.mobile.safe.R;
+import com.mobile.safe.utils.SmsUtils;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Toast;
 
 public class AtoolsActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_atools);
-		
-		
+		setContentView(R.layout.activity_atools);		
 	}
 	
 	/**
@@ -26,5 +28,55 @@ public class AtoolsActivity extends Activity {
 		startActivity(intentv);
 	
 	}	
+	
+	//点击事件 :短信的备份
+		public void smsBackup(View view){
+			
+			if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+				final ProgressDialog pd = new ProgressDialog(this);
+				pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+				pd.setMessage("正在备份。。。。");
+				pd.show();
+				
+				new Thread(){
+					@Override
+					public void run() {
+					try {
+							SmsUtils.backupSms(AtoolsActivity.this,pd);
+							runOnUiThread(new Runnable() {								
+								@Override
+								public void run() {
+									Toast.makeText(AtoolsActivity.this,"短信备份成功",0).show();									
+								}
+							});
+						} catch (Exception e) {
+							e.printStackTrace();
+								runOnUiThread(new Runnable() {
+								
+								@Override
+								public void run() {
+									Toast.makeText(AtoolsActivity.this,"短信备份失败",0).show();									
+								}
+							});
+						}
+							
+					pd.dismiss();
+					
+					}
+				}.start();
+				
+				
+	
+				
+			}
+		}
+		
+	
+		//点击事件 :短信的还原
+		public void smsRestore(View view){
+			
+			
+			
+		}
 		
 }
---
+++ src/com/mobile/safe/utils/SmsUtils.java
@@ -0,0 +1,95 @@
+package com.mobile.safe.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Xml;
+
+/**
+ * 短信的备份和还原 工具类
+ * 短信保存格式
+ * <? xml  version>
+<smss>
+	<sms>
+		<body>你好啊</body>
+		<date>124451566</date>
+		<type>1</type>
+		<address>55568445</address>
+	</sms>
+
+</smss>
+ * 
+ */
+public class SmsUtils {
+	
+	
+	
+	
+	/**
+	 * 短信的备份
+	 * @param context 上下文
+	 * @param pd 进度条对话框
+	 */
+	public static void backupSms(Context context,ProgressDialog pd) throws Exception{
+		ContentResolver resolver = context.getContentResolver();
+		File file = new File(Environment.getExternalStorageDirectory(),"backuo.xml");
+		FileOutputStream fos = new FileOutputStream(file);
+		//把用户的短信一条一条读出来,按照一定的格式写到文件里
+		XmlSerializer serializer = Xml.newSerializer();//获取xml文件的生存期(系列器)
+		//初始化生成器
+				serializer.setOutput(fos,"utf-8");
+				serializer.startDocument("utf-8",true);//true 是否独立
+				serializer.startTag(null,"smss");
+				Uri uri = Uri.parse("content://sms/");
+				Cursor cursor = resolver.query(uri,new String[]{"body","address","type","date"}, null, null, null);
+				//开始备份的时候,设置进度条的最大值
+				int max=cursor.getCount();
+				pd.setMax(max);
+				
+//				backupCallback.beforeSmsBackup(cursor.getCount());
+				int progress = 0;
+				while (cursor.moveToNext()) {
+					Thread.sleep(100);
+					String body = cursor.getString(0);
+					String address = cursor.getString(1);
+					String type = cursor.getString(2);
+					String date = cursor.getString(3);
+					
+					serializer.startTag(null,"sms");
+					
+					serializer.startTag(null,"body");
+					serializer.text(body);
+					serializer.endTag(null,"body");
+					
+					serializer.startTag(null,"address");
+					serializer.text(address);
+					serializer.endTag(null,"address");
+					
+					serializer.startTag(null,"type");
+					serializer.text(type);
+					serializer.endTag(null,"type");
+					
+					serializer.startTag(null,"date");
+					serializer.text(date);
+					serializer.endTag(null,"date");
+					
+					serializer.endTag(null,"sms");
+					//备份过程中,增加进度
+					progress++;
+					pd.setProgress(progress);
+//					backupCallback.onSmsBackup(progress);
+				}
+				serializer.endTag(null,"smss");
+				serializer.endDocument();
+				fos.close();
+	}
+
+}
