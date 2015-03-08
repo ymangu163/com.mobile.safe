27675a7b6843478758be8b08ef0d19acb1f5c979
ymangu163 <ymangu@163.com>
2015-03-08 11:53:31 +0800

    widget小控件

---
+++ src/com/mobile/safe/receiver/MyWidget.java
@@ -0,0 +1,14 @@
+package com.mobile.safe.receiver;
+
+import android.appwidget.AppWidgetProvider;
+
+/*
+ * 创建Widget的步骤：
+① 创建一个类继承 AppWidgetProvider.
+② 因为AppWidgetProvider 是一个广播接收者，要在清单文件中注册这个类.
+③ 在res/下新建一个 xml文件夹，并创建一个<appwidget-provider>为根节点的xml配置文件.
+ */
+
+public class MyWidget extends AppWidgetProvider {
+
+}
